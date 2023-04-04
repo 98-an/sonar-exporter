bfd7c490ad5ff87875b0d4dee21b091b14c2e49e
elwillie <elwilliees@gmail.com>
2023-04-04 08:58:27 +0200

    ✨ feat: Add support for paging

--- app/sonar/sonar.py
+++ app/sonar/sonar.py
@@ -9,35 +9,43 @@ def export_all_sonar_users(sonar_site, sonar_protocol, sonar_domain_name, sonar_
     users_count = 0
 
     print('{} - INFO - Reading Sonar users from Sonar API of {}'.format(datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"), sonar_site))
-    users_response = requests.get(
-        '{}://{}/api/users/search'.format(sonar_protocol, sonar_domain_name),
-        auth=HTTPBasicAuth(sonar_token, "")
-    ).json()
 
-    for user in users_response["users"]:
-        user_login = user.get('login')
-        user_name = user.get('name')
-        user_active = user.get('active')
-        user_tokens_count = user.get('tokensCount')
-        user_local = user.get('local')
-        user_external_identity = user.get('externalIdentity')
-        user_external_provider = user.get('externalProvider')
-        user_email = user.get('email')
-        user_last_connection_date = user.get('lastConnectionDate')
+    response_page = 1
+    while response_page > 0:
+        users_response = requests.get(
+            '{}://{}/api/users/search?p={}'.format(sonar_protocol, sonar_domain_name, response_page),
+            auth=HTTPBasicAuth(sonar_token, "")
+        ).json()
 
-        users_list.append({
-            'date': datetime.datetime.now().strftime("%Y%m%d"),
-            'user_login': user_login,
-            'user_name': user_name,
-            'user_active': user_active,
-            'user_tokens_count': user_tokens_count,
-            'user_local': user_local,
-            'user_external_identity': user_external_identity,
-            'user_external_provider': user_external_provider,
-            'user_email': user_email,
-            'user_last_connection_date': user_last_connection_date
-        })
-        users_count = users_count + 1
+        for user in users_response["users"]:
+            user_login = user.get('login')
+            user_name = user.get('name')
+            user_active = user.get('active')
+            user_tokens_count = user.get('tokensCount')
+            user_local = user.get('local')
+            user_external_identity = user.get('externalIdentity')
+            user_external_provider = user.get('externalProvider')
+            user_email = user.get('email')
+            user_last_connection_date = user.get('lastConnectionDate')
+
+            users_list.append({
+                'date': datetime.datetime.now().strftime("%Y%m%d"),
+                'user_login': user_login,
+                'user_name': user_name,
+                'user_active': user_active,
+                'user_tokens_count': user_tokens_count,
+                'user_local': user_local,
+                'user_external_identity': user_external_identity,
+                'user_external_provider': user_external_provider,
+                'user_email': user_email,
+                'user_last_connection_date': user_last_connection_date
+            })
+            users_count = users_count + 1
+
+        if int(int(users_response["paging"].get("total")) / int(users_response["paging"].get("pageSize"))) >= response_page:
+            response_page = response_page + 1
+        else:
+            response_page = 0
 
     print('{} - INFO - Total: {} users'.format(datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"), str(users_count)))
 
@@ -56,25 +64,33 @@ def export_all_sonar_groups(sonar_site, sonar_protocol, sonar_domain_name, sonar
     groups_count = 0
 
     print('{} - INFO - Reading Sonar groups from Sonar API of {}'.format(datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"), sonar_site))
-    groups_response = requests.get(
-        '{}://{}/api/user_groups/search'.format(sonar_protocol, sonar_domain_name),
-        auth=HTTPBasicAuth(sonar_token, "")
-    ).json()
 
-    for group in groups_response["groups"]:
-        group_id = group.get('id')
-        group_name = group.get('name')
-        group_description = group.get('description')
-        group_default = group.get('default')
+    response_page = 1
+    while response_page > 0:
+        groups_response = requests.get(
+            '{}://{}/api/user_groups/search?p={}'.format(sonar_protocol, sonar_domain_name, response_page),
+            auth=HTTPBasicAuth(sonar_token, "")
+        ).json()
 
-        groups_list.append({
-            'date': datetime.datetime.now().strftime("%Y%m%d"),
-            'group_id': group_id,
-            'group_name': group_name,
-            'group_description': group_description,
-            'group_default': group_default,
-        })
-        groups_count = groups_count + 1
+        for group in groups_response["groups"]:
+            group_id = group.get('id')
+            group_name = group.get('name')
+            group_description = group.get('description')
+            group_default = group.get('default')
+
+            groups_list.append({
+                'date': datetime.datetime.now().strftime("%Y%m%d"),
+                'group_id': group_id,
+                'group_name': group_name,
+                'group_description': group_description,
+                'group_default': group_default,
+            })
+            groups_count = groups_count + 1
+
+        if int(int(groups_response["paging"].get("total")) / int(groups_response["paging"].get("pageSize"))) >= response_page:
+            response_page = response_page + 1
+        else:
+            response_page = 0
 
     print('{} - INFO - Total: {} groups'.format(datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"), str(groups_count)))
 
@@ -89,32 +105,41 @@ def export_all_sonar_groups(sonar_site, sonar_protocol, sonar_domain_name, sonar
 
 
 def export_all_sonar_groups_members(sonar_site, sonar_protocol, sonar_domain_name, sonar_token, groups_list, export_filename):
-
-    print('{} - INFO - Reading Sonar groups members from Sonar API of {}'.format(datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"), sonar_site))
     groups_members_list = []
 
+    print('{} - INFO - Reading Sonar groups members from Sonar API of {}'.format(datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"), sonar_site))
+
     for group in groups_list:
-        group_members_response = requests.get(
-            '{}://{}/api/user_groups/users?name={}'.format(sonar_protocol, sonar_domain_name, group['group_name']),
-            auth=HTTPBasicAuth(sonar_token, "")
-        ).json()
 
         groups_member_count = 0
-        for group_members in group_members_response["users"]:
-            user_login = group_members.get('login')
-            user_name = group_members.get('name')
-            group_name = group['group_name']
-            group_id = group['group_id']
 
-            groups_members_list.append({
-                'date': datetime.datetime.now().strftime("%Y%m%d"),
-                'user_login': user_login,
-                'user_name': user_name,
-                'group_name': group_name,
-                'group_id': group_id
-            })
+        response_page = 1
+        while response_page > 0:
+            group_members_response = requests.get(
+                '{}://{}/api/user_groups/users?name={}&p={}'.format(sonar_protocol, sonar_domain_name, group['group_name'], response_page),
+                auth=HTTPBasicAuth(sonar_token, "")
+            ).json()
 
-            groups_member_count = groups_member_count + 1
+            for group_members in group_members_response["users"]:
+                user_login = group_members.get('login')
+                user_name = group_members.get('name')
+                group_name = group['group_name']
+                group_id = group['group_id']
+
+                groups_members_list.append({
+                    'date': datetime.datetime.now().strftime("%Y%m%d"),
+                    'user_login': user_login,
+                    'user_name': user_name,
+                    'group_name': group_name,
+                    'group_id': group_id
+                })
+
+                groups_member_count = groups_member_count + 1
+
+            if int(int(group_members_response.get("total")) / int(group_members_response.get("ps"))) >= response_page:
+                response_page = response_page + 1
+            else:
+                response_page = 0
 
         print('{} - INFO - Total of {} members for Sonar group {}'.format(datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"), str(groups_member_count), group['group_name']))
 
@@ -131,29 +156,37 @@ def export_all_sonar_projects(sonar_site, sonar_protocol, sonar_domain_name, son
     projects_count = 0
 
     print('{} - INFO - Reading Sonar projects from Sonar API of {}'.format(datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"), sonar_site))
-    projects_response = requests.get(
-        '{}://{}/api/projects/search'.format(sonar_protocol, sonar_domain_name),
-        auth=HTTPBasicAuth(sonar_token, "")
-    ).json()
 
-    for project in projects_response["components"]:
-        project_key = project.get('key')
-        project_name = project.get('name')
-        project_qualifier = project.get('qualifier')
-        project_visibility = project.get('visibility')
-        project_last_analysis_date = project.get('lastAnalysisDate')
-        project_revision = project.get('revision')
+    response_page = 1
+    while response_page > 0:
+        projects_response = requests.get(
+            '{}://{}/api/projects/search?p={}'.format(sonar_protocol, sonar_domain_name, response_page),
+            auth=HTTPBasicAuth(sonar_token, "")
+        ).json()
 
-        projects_list.append({
-            'date': datetime.datetime.now().strftime("%Y%m%d"),
-            'project_key': project_key,
-            'project_name': project_name,
-            'project_qualifier': project_qualifier,
-            'project_visibility': project_visibility,
-            'project_last_analysis_date': project_last_analysis_date,
-            'project_revision': project_revision,
-        })
-        projects_count = projects_count + 1
+        for project in projects_response["components"]:
+            project_key = project.get('key')
+            project_name = project.get('name')
+            project_qualifier = project.get('qualifier')
+            project_visibility = project.get('visibility')
+            project_last_analysis_date = project.get('lastAnalysisDate')
+            project_revision = project.get('revision')
+
+            projects_list.append({
+                'date': datetime.datetime.now().strftime("%Y%m%d"),
+                'project_key': project_key,
+                'project_name': project_name,
+                'project_qualifier': project_qualifier,
+                'project_visibility': project_visibility,
+                'project_last_analysis_date': project_last_analysis_date,
+                'project_revision': project_revision,
+            })
+            projects_count = projects_count + 1
+
+        if int(int(projects_response["paging"].get("total")) / int(projects_response["paging"].get("pageSize"))) >= response_page:
+            response_page = response_page + 1
+        else:
+            response_page = 0
 
     print('{} - INFO - Total: {} projects'.format(datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"), str(projects_count)))
 
